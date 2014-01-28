612dc794eef26e662b11ce08bef04dff5330d130
m.lewandowski <m.lewandowski@cksource.com>
2014-01-28 14:25:39 +0100

    Added validation to dialog, to prevent unsupported oembed content urls.

--- plugins/mediaembed/lang/en.js
+++ plugins/mediaembed/lang/en.js
@@ -4,6 +4,7 @@ For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'mediaembed', 'en', {
 	button: 'Insert Media Embed',
+	invalidUrl: 'Given url is not supported by Media Embed',
 	title: 'Media Embed',
 	url: 'URL'
 } );
